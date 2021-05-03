79328c5928a41011112834ebd2498d9cb17d8be3
Nick Verwymeren <nick@teamgi.ca>
2021-05-03 08:52:02 -0500

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       options: {
         host: 'https://www.graphicintuitions.com',
         sitemap: 'https://www.graphicintuitions.com/sitemap.xml',
-        policy: [{ userAgent: '*', disallow: '/tags/', allow: '/' }]
+        policy: [{ userAgent: '*', disallow: '*' }]
       }
     },
     // {
